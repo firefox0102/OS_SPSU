6e078cacb767d338aa0bdaceb43bdd2cd7225b6d
firefox0102 <firefox0102@gmail.com>
2014-02-03 18:15:35 -0500

    Uploaded the Classes and Project Frame

---
+++ OS Project/OS Project/Classes/CPU Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public class CPU
+{
+	public CPU()
+	{
+        Console.Write("hello world????");
+	}
+
+
+    public void Fetch()
+    {
+
+    }
+
+    public void Decode()
+    {
+
+    }
+
+    public void Execute()
+    {
+
+    }
+}
---
+++ OS Project/OS Project/Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OS_Project
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Generic stuff that allows the window to show up
+            /*Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(true);
+            Application.Run(new Form1());
+            */
+            
+        }
+    }
+}
