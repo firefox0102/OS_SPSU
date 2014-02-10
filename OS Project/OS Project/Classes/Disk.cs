fa24a1398d1bd04e73b73114a63c0dbd1b73c67d
tscoville2012 <tyler.Scoville@yahoo.com>
2014-02-10 16:06:12 -0500

    Update Disk.cs
    Trying to get the project started. What I think the disk should do. Loader should put hex codes from file and make them binary before passing as individual integers to disk. It should also store information about jobs in PCB. CODE PROBABLY DOES NOT WORK.

--- OS Project/OS Project/Classes/Disk.cs
+++ OS Project/OS Project/Classes/Disk.cs
@@ -8,9 +8,22 @@ namespace OS_Project
 {
     class Disk
     {
-        public Disk()
+        List<int> Disk {get; set;}
+        
+        void AddToDisk(int Data)
         {
-
+            Disk.add(Data);
         }
+        
+        GetData(int DataStart, int DataSize)
+        {
+            list<int> FromDisk = new List<int>();
+            
+            for(int i = DataStart; i < (DataStart + DataSize); i++)
+                FromDisk.add(Disk[i]);
+                
+            return FromDisk;
+        }
+    
     }
 }
