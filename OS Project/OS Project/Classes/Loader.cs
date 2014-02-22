36b783aa55cb5568d516c5338a2a2d5b172f4ab3
jcathcart1 <jcathcart1@gmail.com>
2014-02-21 19:28:51 -0500

    Update Loader.cs

--- OS Project/OS Project/Classes/Loader.cs
+++ OS Project/OS Project/Classes/Loader.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace OS_Project
 {
     public class Loader
     {
-        public Loader()
+        StreamReader sr;
+        PCB pcb;
+        
+        public Loader(p)
         {
-
+            StreamReader sr = new Streamreader();
+            pcb = p;
+        }
+        
+        string getFromFile()
+        {
+            if(!sr.eof())
+            {
+                if(/*I didn't hit the next// marks*/)
+                {
+                    Process temp = new Process();
+                    storeInPCB(pcb, temp);
+                }
+            }
+        }
+        
+        string convertToBinaryString()
+        {
+            
+        }
+        
+        void storeInPCB(p)
+        {
+           //store a reference of the process in the pcb
         }
     }
 }
