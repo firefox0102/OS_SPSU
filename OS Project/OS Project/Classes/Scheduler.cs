4ebb549ff98e0ddd3a44e25b0c5651af49fc6c02
jcathcart1 <jcathcart1@gmail.com>
2014-02-21 19:59:17 -0500

    Update Scheduler.cs

--- OS Project/OS Project/Classes/Scheduler.cs
+++ OS Project/OS Project/Classes/Scheduler.cs
@@ -8,9 +8,38 @@ namespace OS_Project
 {
     class Scheduler
     {
+        LongTermScheduler lts;
+        ShortTermScheduler sts;
+        
         public Scheduler()
         {
-
+            lts = new LongTermScheduler();
+            sts = new ShortTermScheduler();
+        }
+        
+        //make sure MAIN calls the update continuously
+        public void Update()
+        {
+            LongTermScheduler.Update();
+            ShortTermScheduler.Update();
+        }
+    }
+    
+    class LongTermScheduler()
+    {
+        //get a job from the disk, pass that job to RAM IF SPACE IF AVAILABLE
+        public void Update()
+        {
+            //update logic   
+        }
+    }
+    
+    class ShortTermScheduler()
+    {
+        //pulls off of RAM and passes to the dispatcher
+        public void Update()
+        {
+            //update logic
         }
     }
 }
