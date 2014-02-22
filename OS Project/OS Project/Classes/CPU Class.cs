f4078acc9797ec10cb563221d4da0bacd47148e7
tscoville2012 <tyler.Scoville@yahoo.com>
2014-02-22 13:12:32 -0500

    shitbroke

--- OS Project/OS Project/Classes/CPU Class.cs
+++ OS Project/OS Project/Classes/CPU Class.cs
@@ -39,29 +39,29 @@ namespace OS_Project.Classes
             //is passed pcb id then gets pcb info
             //FILLS instructionList
            // Dispatcher.Instance.sendProcess();
-            instructionList = new List<string>(new string[] {   "0xC050005C",
-                                                                "0x4B060000",
-                                                                "0x4B010000",
-                                                                "0x4B000000",
-                                                                "0x4F0A005C",
-                                                                "0x4F0D00DC",
-                                                                "0x4C0A0004",
-                                                                "0xC0BA0000",
-                                                                "0x42BD0000",
-                                                                "0x4C0D0004",
-                                                                "0x4C060001",
-                                                                "0x10658000",
-                                                                "0x56810018",
-                                                                "0x4B060000",
-                                                                "0x4F0900DC",
-                                                                "0x43970000",
-                                                                "0x05070000",
-                                                                "0x4C060001",
-                                                                "0x4C090004",
-                                                                "0x10658000",
-                                                                "0x5681003C",
-                                                                "0xC10000AC",
-                                                                "0x92000000" });
+            instructionList = new List<string>(new string[] {   "C050005C",
+                                                                "4B060000",
+                                                                "4B010000",
+                                                                "4B000000",
+                                                                "4F0A005C",
+                                                                "4F0D00DC",
+                                                                "4C0A0004",
+                                                                "C0BA0000",
+                                                                "42BD0000",
+                                                                "4C0D0004",
+                                                                "4C060001",
+                                                                "10658000",
+                                                                "56810018",
+                                                                "4B060000",
+                                                                "4F0900DC",
+                                                                "43970000",
+                                                                "05070000",
+                                                                "4C060001",
+                                                                "4C090004",
+                                                                "10658000",
+                                                                "5681003C",
+                                                                "C10000AC",
+                                                                "92000000" });
 
         }
 
@@ -74,7 +74,7 @@ namespace OS_Project.Classes
         {
             for(int i = 0; i<instructionList.Count; i++ )
             {
-                currentProcess = instructionList[i];
+                currentProcess = Convert.ToString(Convert.ToInt32(instructionList[i],16),2);
                 string instructionFormat = currentProcess.Substring(0, 2);
 
                 if (instructionFormat == "00")
