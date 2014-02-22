a86b5e099e193d2a1ece59648e251754d7db1a44
jcathcart1 <jcathcart1@gmail.com>
2014-02-22 14:13:01 -0500

    Modified Loader.cs

--- OS Project/OS Project/Classes/Loader.cs
+++ OS Project/OS Project/Classes/Loader.cs
@@ -1,36 +1,50 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using OS_Project.Classes;
 namespace OS_Project
 {
     public class Loader
     {
+        /// <summary>
+        /// The loader will load the file information onto the disk
+        /// the disk will create a process with the object variables
+        /// - Job Number
+        /// - Priority
+        /// - Job length
+        /// - List for instructions
+        /// - List for data
+        /// The file lines will be parsed into 32 bit lenth binary
+        /// except for first three variables which are created first
+        /// from the first commented lines of the file.
+        /// </summary>
+
         StreamReader sr;
         
         public Loader()
         {
-            StreamReader sr = new StreamReader();
+            StreamReader sr = new Streamreader();
         }
         
         string getFromFile()
         {
-            if(!sr.eof())
-            {
-                if(/*I didn't hit the next// marks*/)
-                {
-                    //dont forget to create the process class
+            if( !sr.eof() ){
+                
                     Process temp = new Process();
                 }
             }
-        }
         
-        string convertToBinaryString()
-        {
+        string convertToBinaryString(string temp){
+            // Strips the hexadecimal tag from the front of lines
+            // Converts the hex to binary
+            // Casts the binary as a string         
+            string convertedText = temp.Substring(2);
             
+
+            return convertedText;
         }
     }
 }
