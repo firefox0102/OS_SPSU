84f616095199bd09df4a9ffcf66e962fd7aacb11
jcathcart1 <jcathcart1@gmail.com>
2014-02-21 20:11:48 -0500

    Update Memory.cs

--- OS Project/OS Project/Classes/Memory.cs
+++ OS Project/OS Project/Classes/Memory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace OS_Project
 {
-    class Memory
+    public class Memory
     {
+        const int maxSize = 1024;
+        List<List<string>> memory 
+        
+        public Memory()
+        {
+            memory = new Memory();
+        }
+        
+        string get(int index, int pc)
+        {
+            return memory[index, pc];
+        }
+        
+        void set(List<string> temp)
+        {
+            memory.Add(temp);
+        }
+        
+        void remove(int index)
+        {
+            memory.Remove[index];
+        }
     }
 }
