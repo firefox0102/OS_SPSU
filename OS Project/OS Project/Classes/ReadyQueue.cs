6a5d9e32c8cc24d4046d4c79f448e7d8db356710
Andrew Pfundstein <drew@myskiprofile.com>
2014-02-22 01:55:21 -0500

    added ready Queue  class

---
+++ OS Project/OS Project/Classes/ReadyQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Project.Classes
+{
+    class ReadyQueue
+    {
+        private static ReadyQueue q;
+        List<int> readyQ = new List<int>();
+
+        protected ReadyQueue() { }
+        public static ReadyQueue GetQueue()
+        {
+            if (q == null)
+            {
+                q = new ReadyQueue();
+            }
+            return q;
+        }
+
+        public void addProcess(int ProcessId)
+        {
+            readyQ.Add(ProcessId);
+        }
+        public void removeProcess(int ProcessId)
+        {
+            readyQ.Remove(ProcessId);
+        }
+
+        public int getNextJob()
+        {
+            int i = readyQ[0];//gets first element
+            readyQ.RemoveAt(0);//removed first element like a pop front
+            return i;
+
+        }
+
+        public int getQueueCount()
+        {
+            return readyQ.Count();
+        }
+
+        
+
+
+    }
+}
