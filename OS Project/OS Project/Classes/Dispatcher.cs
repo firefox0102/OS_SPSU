8a20af73b1c166e5950dbe421e2c37cf66559d5b
Andrew Pfundstein <drew@myskiprofile.com>
2014-02-22 02:14:39 -0500

    yerp

--- OS Project/OS Project/Classes/Dispatcher.cs
+++ OS Project/OS Project/Classes/Dispatcher.cs
@@ -17,5 +17,21 @@ namespace OS_Project
             
         }
         //Drew will write the dispatcher
+        public void sendProcess(int processId)
+        {
+            //get from pcb
+            //send to cpu/set values for cpu to read
+            idle = false;
+        }
+        //dispatcher seems to send instuctions one at a time to the cpu. we can implement this by calling a get current instuction function or some other way
+        public bool isIdle()
+        {
+            return idle;
+        }
+        public void terminateProcess()
+        {
+           //get current process start and end and then clear ram
+        }
+
     }
 }
