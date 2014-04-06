779626cfef83faf194b17a1b1b58a0fce3db662e
jcathcart1 <jcathcart1@gmail.com>
2014-04-06 15:47:45 -0400

    modified PCB

--- OS Project/OS Project/Classes/PCB.cs
+++ OS Project/OS Project/Classes/PCB.cs
@@ -14,66 +14,61 @@ namespace OS_Project{
         public int pc;
         public int priority;
         // Will contain the indexes of frames for instructions
-        public int[] memInstrPos;
+        public List<int> logical_memInstr;
         // Will contain the indexes of frames for data
-        public int[] memDataPos;
+        public List<int> logical_memDataIn;
+        public List<int> logical_memDataOut;
+        public List<int> pageTable; 
 
         public int instrLength;
-        public int dataLength;
-        // Instructions and Data together
         public int totalLength;
-        public int sizeInBytes;
 
-        public int[] diskInstrStartPos; // now an array of 2 coordinates
-        public int[] diskInstrEndPos;   // now an array of 2 coordinates
-        public int[] diskDataStartPos;  // now an array of 2 coordinates
-        public int[] diskDataEndPos;    // now an array of 2 coordinates
+        public int diskInstrStart; 
+        public int diskInstrEnd;
+        public int diskDataInStart;   
+        public int diskDataInEnd;  
+        public int diskDataOutStart;  
+        public int diskDataOutEnd;
 
         public enum Status { error, created, ready, waiting, running, terminated }; //created instead of new since new is resered word
         public Status state;
         public Stopwatch elapsedTime;
         public Stopwatch waitingTime;
-        public int[] registers;
 
-        public PCB(int id, int priority, int[] startPos){
+        public PCB(int id, int priority, int startPos){
             this.id = id;
             pc = 0;
             this.priority = priority;
 
-            diskInstrStartPos = new int[2];
-            diskInstrEndPos = new int[2];
-            diskDataStartPos = new int[2];
-            diskDataEndPos = new int[2];    
+            diskInstrStart = startPos;
+            diskInstrEnd = -1;
+            diskDataInStart = -1;
+            diskDataInEnd = -1;
+            diskDataOutStart = -1;    
+            diskDataOutEnd = -1;
 
-            memInstrPos =  new int[2];
-            memDataPos = new int[2];
+            logical_memInstr = new List<int>(0);
+            logical_memDataIn = new List<int>(0) ;
+            logical_memDataOut = new List<int>(0);
+            pageTable = new List<int>(0);
 
             instrLength = -1;
-            dataLength = -1;
             totalLength = -1;
-            sizeInBytes = -1;
-
-            diskInstrStartPos[0] = startPos[0];
-            diskInstrStartPos[1] = startPos[1];
-
             state = Status.created;
             elapsedTime = new Stopwatch();
             waitingTime = new Stopwatch();
-            registers = new int[16];
-            for (int i = 0; i < 16; i++){
-                registers[i] = 0;
-            }
-
         }
 
         public void printPCBInfo(){
             Console.WriteLine("ID : "+id );
             Console.WriteLine("PC : "+pc );
             Console.WriteLine("Priority   : "+priority );
-            Console.WriteLine("Disk Instructions Start   : "+diskInstrStartPos[0]+","+diskInstrStartPos[1] );
-            Console.WriteLine("Disk Intructions End   : "+ diskInstrEndPos[0]+","+diskInstrEndPos[1]);
-            Console.WriteLine("Disk Data Start   : "+diskDataStartPos[0]+","+diskDataStartPos[1] );
-            Console.WriteLine("Disk Data End   : "+diskDataEndPos[0]+","+diskDataEndPos[1] );
+            Console.WriteLine("Disk Instructions Start   : "+diskInstrStart);
+            Console.WriteLine("Disk Intructions End   : "+ diskInstrEnd);
+            Console.WriteLine("Disk DataIn Start   : "+diskDataInStart);
+            Console.WriteLine("Disk DataIn End   : "+diskDataInEnd );
+            Console.WriteLine("Disk DataOut Start   : " + diskDataOutStart);
+            Console.WriteLine("Disk DataOut End   : " + diskDataOutEnd);
         }
 
     }
